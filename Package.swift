63a9f54ea811a45c4a92893ef274d01b01c7fcbe
Casper Rasmussen <cr@nodes.dk>
2016-11-13 18:04:59 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
         name: "Meta",
         dependencies: [
-                .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1)
+               
         ]
-        )
+)
