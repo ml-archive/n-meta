33ef836881562c5cdc7b2da15307597d88ed13a8
Casper Rasmussen <cr@nodes.dk>
2016-11-13 17:55:25 +0100

    added package swift

---
+++ Package.swift
@@ -0,0 +1,8 @@
+import PackageDescription
+
+let package = Package(
+        name: "Meta",
+        dependencies: [
+                .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1)
+        ]
+        )
