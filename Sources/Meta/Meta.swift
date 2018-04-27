d2c710b454531e4caded4f6f4a07739f31f252be
Casper Rasmussen <cr@nodes.dk>
2018-04-27 11:21:32 +0200

    Renaming package to NMeta

--- Sources/Meta/Meta.swift
+++
@@ -1,3 +0,0 @@
-struct meta {
-    var text = "Hello, World!"
-}
---
+++ Tests/NMetaTests/NMetaTests.swift
@@ -0,0 +1,13 @@
+import XCTest
+@testable import NMeta
+
+class NMetaTests: XCTestCase {
+    func testExample() {
+        XCTAssertEqual(true, true)
+    }
+
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
