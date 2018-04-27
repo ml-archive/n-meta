45b0dc37e289b2aa4729b9fe33fa11e49d5cdc7e
Casper Rasmussen <cr@nodes.dk>
2018-04-27 16:54:53 +0200

    Changed to a provider instead

---
+++ Sources/NMeta/NMetaProvider.swift
@@ -0,0 +1,25 @@
+import Service
+import Vapor
+
+public final class NMetaProvider {
+
+    public let config: NMetaConfig
+    
+    public init(config: NMetaConfig) {
+        self.config = config
+    }
+}
+
+// MARK: - Provider
+extension NMetaProvider: Provider {
+    public func register(_ services: inout Services) {
+        services.register(config)
+        services.register { _ in
+            return NMetaContainerService()
+        }
+    }
+    
+    public func didBoot(_ container: Container) -> EventLoopFuture<Void> {
+        return .done(on: container)
+    }
+}
