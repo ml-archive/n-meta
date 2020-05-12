139bd82a251e76095bab363970f7b4da056745e1
Emil Marashliev <emarashliev@users.noreply.github.com>
2020-05-12 15:40:15 +0200

    Update Package.swift
    Co-authored-by: Siemen Sikkema <siemensikkema@users.noreply.github.com>

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
 
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.5.0")
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0")
     ],
     targets: [
         .target(
